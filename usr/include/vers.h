61b4d8097f51074c9508eeda5742d3996d99542f
0x16000 <openbsd.gnu@gmail.com>
2025-06-05 16:07:27 +0200

    Update vers.h

--- usr/include/vers.h
+++ usr/include/vers.h
@@ -5,7 +5,7 @@
 #define OS_NAME "Unics"
 #define OS_HOSTNAME "root"
 #define OS_ARCH "i386"
-#define OS_BUILD_VERSION "0.1-20250522"
+#define OS_BUILD_VERSION "0.1-20250605"
 #define OS "Unics"
 
 #endif /* _VERS_H */
