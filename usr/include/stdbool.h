4652ff794154fb30abd20baf488bbd348f8d3e13
0x16000 <openbsd.gnu@gmail.com>
2025-06-20 17:27:43 +0200

    More implementations (enough for today)

--- usr/include/stdbool.h
+++ usr/include/stdbool.h
@@ -1,90 +1,46 @@
-#ifndef _STDBOOL_H
-#define _STDBOOL_H 1
+/* $OpenBSD: stdbool.h,v 1.7 2015/09/04 23:47:09 daniel Exp $ */
+/* $Unics: stdbool.h,v 1.7 2025/06/17:26:32 $ */
 
-/* Prevent C++ name mangling */
-#ifdef __cplusplus
-extern "C" {
-#endif
+/*
+ * Written by Marc Espie, September 25, 1999
+ * Public domain.
+ */
 
-/* Check if we're using a C99 or later compiler */
-#if !defined(__STDC_VERSION__) || (__STDC_VERSION__ < 199901L)
-    #if !defined(__cplusplus)
-        #warning "This header requires C99 or later."
-    #endif
-#endif
+#ifndef	_STDBOOL_H_
+#define	_STDBOOL_H_	
 
-/* Handle GCC-specific attributes and optimizations */
-#ifdef __GNUC__
-    #define _BOOL_INLINE __inline__ __attribute__((always_inline))
-    #define _BOOL_UNUSED __attribute__((unused))
-#else
-    #define _BOOL_INLINE
-    #define _BOOL_UNUSED
-#endif
-
-/* Ensure proper bool type definition */
 #ifndef __cplusplus
-    #if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
-        /* C99 or later - use _Bool as per standard */
-        #define bool _Bool
-    #else
-        /* Pre-C99 compatibility */
-        typedef unsigned char bool;
-    #endif
+
+#if defined(__GNUC__) || \
+	(defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901)
+/* Support for C99: type _Bool is already built-in. */
+#define false	0
+#define true	1
+
+#else
+/* `_Bool' type must promote to `int' or `unsigned int'. */
+typedef enum {
+	false = 0,
+	true = 1
+} _Bool;
+
+/* And those constants must also be available as macros. */
+#define	false	false
+#define	true	true
+
 #endif
 
-/* Define standard boolean constants */
-#define true 1
-#define false 0
+/* User visible type `bool' is provided as a macro which may be redefined */
+#define bool _Bool
+
+#else /* __cplusplus */
+#define _Bool 	bool
+#define bool 	bool
+#define false 	false
+#define true 	true
+#endif /* __cplusplus */
+
+/* Inform that everything is fine */
 #define __bool_true_false_are_defined 1
 
-/* Additional safety and convenience macros */
-#define BOOL_TO_STR(x) ((x) ? "true" : "false")
-#define IS_BOOL(x) __builtin_types_compatible_p(typeof(x), bool)
-
-/* Boolean validation macro (compile-time check for boolean type) */
-#define VALIDATE_BOOL(x) \
-    _Static_assert(IS_BOOL(x), "Expression must be of type bool")
-
-/* Optimized boolean operations */
-static _BOOL_INLINE bool bool_and(bool a, bool b) _BOOL_UNUSED;
-static _BOOL_INLINE bool bool_or(bool a, bool b) _BOOL_UNUSED;
-static _BOOL_INLINE bool bool_xor(bool a, bool b) _BOOL_UNUSED;
-static _BOOL_INLINE bool bool_not(bool a) _BOOL_UNUSED;
-
-static _BOOL_INLINE bool bool_and(bool a, bool b) {
-    return (bool)(a && b);
-}
-
-static _BOOL_INLINE bool bool_or(bool a, bool b) {
-    return (bool)(a || b);
-}
-
-static _BOOL_INLINE bool bool_xor(bool a, bool b) {
-    return (bool)(a ^ b);
-}
-
-static _BOOL_INLINE bool bool_not(bool a) {
-    return (bool)(!a);
-}
-
-/* Size verification for bool type */
-_Static_assert(sizeof(bool) == 1, "Bool must be 1 byte in size");
-
-/* Debug assertion macros (only active in debug builds) */
-#ifdef DEBUG
-    #define ASSERT_BOOL(x) \
-        do { \
-            if (!IS_BOOL(x)) { \
-                __builtin_trap(); \
-            } \
-        } while(0)
-#else
-    #define ASSERT_BOOL(x) ((void)0)
-#endif
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* _STDBOOL_H */
+#endif /* _STDBOOL_H_ */
