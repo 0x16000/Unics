41b2aff339f5f30dc5da2071f860b240758c8103
0x16000 <openbsd.gnu@gmail.com>
2025-06-22 13:15:57 +0200

    fix output formatting in ls command

--- bin/ls.c
+++ bin/ls.c
@@ -13,9 +13,11 @@ int ls_main(int argc, char *argv[]) {
         File *f = &root_fs.files[i];
         // Only show files in current directory
         if (f->parent == current_dir) {
-            printf("%s%s\n", f->name, f->is_dir ? "/" : "");
+            printf("%s%s ", f->name, f->is_dir ? "/" : "");
         }
     }
+
+    printf("\n");  // newline after all entries
 
     return 0;
 }
